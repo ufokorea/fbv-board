8e0bcc08ecdeed3cd6e9b74f4a96f5332738a89c
ufokorea <seetheblue@naver.com>
2024-12-08 14:36:10 +0900

    second

--- board/urls.py
+++ board/urls.py
@@ -1,1 +1,10 @@
 from django.urls import path
+
+from . import views
+app_name = 'board'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('list/', views.list, name='list'), 
+    path('read/<int:id>/', views.read, name='read'),        
+]
--- board/views.py
+++ board/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'board/index.html')
+
+from .models import Board
+
+def list(request):
+    board_list =Board.objects.all()
+    context = {
+        'board_list' : board_list,
+    }
+    return render(
+        request, 
+        'board/list.html', 
+        context
+    )
+    
+def read(request, id):
+    board = Board.objects.get(pk=id)
+    board.incrementReadCount()
+    return render(request, 'board/read.html', {'board':board})
+
